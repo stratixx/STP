e49673ff0098dcd5d7268f8c3af26cd5a7c59b08
Konrad W <darnokusex@wp.pl>
2018-04-21 15:47:13 +0200

    skrypty i symulacje do 9 i 10

--- projekt 1/zad_10/zad_10.m
+++ projekt 1/zad_10/zad_10.m
@@ -1,4 +1,4 @@
-load('../dane_po_zad_6.mat');
+load('../dane_po_zad_8.mat');
 
 x0 = [-1 -2 -3];
 x0_observer = [ 0 0 ]; % stan poczatkowy obserwatora
@@ -10,10 +10,10 @@ A21 = A2(2:3,1);
 B_1 = B2(1,1);
 B_2 = B2(2:3,1);
 
-tkonc = 1;
+tkonc = 2;
 
 %zaladowanie modelu symulowanego
-model = 'observer_test';
+model = 'reg_state_from_observer';
 load_system(model);
 
 cs = getActiveConfigSet(model);
@@ -23,13 +23,11 @@ set_param(model_cs,'Solver','ode45',...
                 'SaveState','on', 'SaveOutput','on');
 
 if true   
-    z2 = 0;
-    z3 = 0;
-    for kz2=1:1:length(z2)
-        for kz3=1:1:length(z3)
-            z0_observer = [z2(kz2) z3(kz3)];
-            L=acker(A22',A12', z0_observer)';
+    
+    for kK=1:1:length(K(:,1))
+        for kL=1:1:length(L(1,:))
             sim_model = sim(model, model_cs);
+            y = sim_model.get('yout');
             x = sim_model.get('xout');
             t = sim_model.get('tout');
             if  max(max(x))>10^3
@@ -37,31 +35,30 @@ if true
                 continue;
             end
             
-            x2_reg = x(:,3);
-            x2_obs = x(:,4);
-            x3_reg = x(:,2);
-            x3_obs = x(:,5);
+                %continue;
+            x1 = x(:,1);
+            x2 = x(:,2);
+            x3 = x(:,3);
+            u  = y(:,2);
             
-            display(z2(kz2));
-            display(z3(kz3));
             figure(1)
             subplot(2,1,1);
             xlabel('Czas');
             ylabel('Wartoœæ');
             hold on; box on; grid on;
-            title('Zmienna stanu x2');
-            plot(t, x2_reg);
-            plot(t, x2_obs);
-            legend('x_2reg(t)', 'x_2obs(t)', 'Location', 'southeast');
+            title('Sygna³ steruj¹cy');
+            plot(t, u);
+            legend('u(t)');
             subplot(2,1,2);
             xlabel('Czas');
             ylabel('Wartoœæ');
             hold on; box on; grid on;
-            title('Zmienna stanu x3');
-            plot(t, x3_reg);
-            plot(t, x3_obs);
-            legend('x_3reg(t)', 'x_3obs(t)', 'Location', 'southeast');
-            print(strcat('img/ogolne/zad_8_z2_', strrep(num2str(z2(kz2)),'.','_'), '_z3_', strrep(num2str(z2(kz2)),'.','_'),'_tkonc_',num2str(tkonc)),'-dpng');
+            title('Zmienne stanu');
+            plot(t, x1);
+            plot(t, x2);
+            plot(t, x3);
+            legend('x_1(t)', 'x_2(t)', 'x_3(t)');
+            print(strcat('img/ogolne/zad_10_kK_', num2str(kK), '_kL_', num2str(kL),'_tkonc_',num2str(tkonc)),'-dpng');
             hold off;
             close all;
         end

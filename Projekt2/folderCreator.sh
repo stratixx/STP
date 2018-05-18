d0f75318c3a89f70b95b21e95340f4f230d33ac4
Konrad W <darnokusex@wp.pl>
2018-05-18 09:53:30 +0200

    folderCreator ;)

---
+++ Projekt2/folderCreator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+counter=1
+while [ $counter -le 6 ]
+do
+	echo Creating zad_$counter/
+	mkdir zad_$counter/
+	touch zad_$counter/zad_$counter.m
+	git add zad_$counter/.
+	((counter++))
+done
+git commit -m 'auto create folders'
+echo All done
